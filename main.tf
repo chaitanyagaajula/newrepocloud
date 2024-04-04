bb29dd644dccd223a3562415aa78f327d66175b4
Chaitanya R Gaajula <chaitanya@gaajula.com>
2024-04-04 19:27:32 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  credentials = file("/root/terraform-key.json")
+  project     = "prj-35-418015"
+  region      = "us-central1" # Set your desired region
+}
+
+
+resource "google_compute_instance" "example" {
+  name         = "test"
+  machine_type = "e2-micro"
+  zone         = "us-central1-b"
+  allow_stopping_for_update = "true"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
+
+
